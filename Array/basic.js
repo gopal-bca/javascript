1980573fd857573376916788bea779d584066d5b
gopal-bca <gopigorain25@gmail.com>
2024-01-16 13:29:17 +0530

    about type of array & length

--- Array/basic.js
+++ Array/basic.js
@@ -1,4 +1,8 @@
 // Array
 let marks = [97, 82 , 75 , 64 , 36];
 console.log(marks);
+console.log(marks.length); //property
+console.log(typeof marks);
 
+let anotherNameOfLordKrishna = ["Gopal" , "Govind" , "Kanha"];
+console.log(anotherNameOfLordKrishna);
