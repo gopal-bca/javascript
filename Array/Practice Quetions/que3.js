327d45c93b3698d8082c94f305619b30b28b6142
gopal-bca <gopigorain25@gmail.com>
2024-01-17 20:28:27 +0530

    Practise

---
+++ Array/Practice Quetions/que3.js
@@ -0,0 +1,17 @@
+//Create an array to store companies -> "Bloomberg", "microsoft","Uber","Google","IBM","Netflix".
+// // (a) Remove the first company from the array
+
+// let arr = ["Bloomberg", "microsoft","Uber","Google","IBM","Netflix"];
+// arr.splice(0,1);
+// console.log(arr);
+
+// (b)Remove uber & Add Ola in its place
+let arr = ["Bloomberg", "microsoft","Uber","Google","IBM","Netflix"];
+arr.splice(2,1,"Ola");
+console.log(arr);
+
+// //(c)Add Amezon at the end
+// let arr = ["Bloomberg", "microsoft","Uber","Google","IBM","Netflix"];
+// arr.splice(5,0,"Amezon");
+// console.log(arr);
+
