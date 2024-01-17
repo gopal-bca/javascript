49121f09f1948ee2afa2a607318bd968ff8c3ecf
gopal-bca <gopigorain25@gmail.com>
2024-01-17 20:06:34 +0530

    Array methods

---
+++ Array/ArrayMethods.js
@@ -0,0 +1,43 @@
+let foodItems = ["potato", "apple", "lichi" , "tomato"];
+let marks = [97 , 88 ];
+// //Push()
+// foodItems.push("chips" , "burger" , "paneer");
+// console.log(foodItems);
+
+// //Pop
+// console.log(foodItems);
+// let deletedItems = foodItems.pop();
+// console.log(foodItems);
+// console.log("deleted =",deletedItems);
+
+// //toString
+// console.log(foodItems.toString());
+// console.log(marks.toString());
+
+// //Concat() : joins multiple arrays & returns result
+// let marvel_heroes = ["thor", "spiderman" , "ironman"];
+// let dc_heroes = ["superman" , "batman"];
+// let heroes = marvel_heroes.concat(dc_heroes);
+// console.log(heroes);
+
+// //unshift() : add to start and shift
+// let marvelHeroes = ["thor", "spiderman" , "ironman"];
+
+// marvelHeroes.unshift("antman");
+// console.log(marvelHeroes);
+
+// let val = marvelHeroes.shift();
+// console.log("DELETED",val);
+// console.log(marvelHeroes);
+
+// //slice() : returns a piece of the array 
+// // syntex : slice(startIdx,endIdx)
+// let marvelHeroes = ["thor", "spiderman" , "ironman" , "antman" , "Dr. strange"];
+// console.log(marvelHeroes.slice(1,4));
+
+//splice() : change original array(add.remove,replace)
+//syntex : splice(startIdx,delCount,newEI1...)
+
+// let arr = [1, 2, 3, 4, 5, 6, ,7];
+// arr.splice(2, 2, 101, 102 ,103);
+// console.log (arr);
